e5b67cb391fd43c633c306eb6d77bae1c9faef84
AttAditya <attachment.aditya@gmail.com>
2024-06-25 23:59:46 +0530

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import logo from './assets/Logo.png';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
+            <img src={logo} className="App-logo" alt="logo" />
+            
             <h1>
                 This will be the official website for <em>the TED Talk Tribe</em>.
             </h1>
