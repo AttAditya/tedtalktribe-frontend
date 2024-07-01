3508b0734ec077c84016b16abba9b3e8d4929a8c
AttAditya <attachment.aditya@gmail.com>
2024-07-01 09:17:57 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Calendar } from './sections';
 
 function App() {
     return (
         <div className="App">
-            <Calendar />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={(
+                        <div>
+                            <h1>Landing Page</h1>
+                            <h2>Site Indexing</h2>
+                            <ul>
+                                <li><Link to="/events">Events</Link></li>
+                                <li><Link to="/login">Login</Link></li>
+                                <li><Link to="/signup">Signup</Link></li>
+                            </ul>
+                        </div>
+                    )} />
+                    <Route path="/events" element={"Events Page"} />
+                    <Route path="/events/:id" element={"Event Detail Page"} />
+                    <Route path="/login" element={"Login Page"} />
+                    <Route path="/signup" element={"Signup Page"} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
